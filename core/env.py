7fb0fdb7ddda2182ce965d641ff1a942550737f7
rubbie kelvin voltsman <rubbiekelvinvoltsman@gmail.com>
2022-08-06 07:19:38 +0100

    chore: added env

---
+++ core/env.py
@@ -0,0 +1,18 @@
+import os
+import dotenv
+from typing import cast
+from .constants import BASE_DIR
+
+dotenv.load_dotenv(BASE_DIR / '.env')
+
+env = lambda x : {
+        **dotenv.dotenv_values,
+        **os.environ # system environ should override .env
+    }.get(x)
+
+class Env:
+    SECRET_KEY: str = env('SECRET_KEY')
+    DEBUG: bool = bool(env('DEBUG'))
+    ALLOWED_HOSTS: list[str] = cast(str, env('ALLOWED_HOSTS') or '*').split(',')
+
+
